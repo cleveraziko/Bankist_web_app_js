539dc7337babe047558d7b02c5c0dfd195c298fb
cleveraziko <74561277+cleveraziko@users.noreply.github.com>
2021-03-03 23:18:58 +0500

    ...coding

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
+
 
 const openModal = function () {
 	modal.classList.remove('hidden');
@@ -61,9 +66,50 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 	}
 	;
 
+});
+
+
+tabsContainer.addEventListener('click', function (e) {
+	const clicked = e.target.closest('.operations__tab');
+	// console.log(clicked);
+	if (!clicked) return;
+
+	tabs.forEach(t => t.classList.remove);
+	clicked.classList.add('operations__tab--active');
+	tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+	// console.log(clicked.dataset.tab)
+	document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+
+});
+
+
+const handleHover = function (e) {
+	if (e.target.classList.contains('nav__link')) {
+		const link = e.target;
+		const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+		const logo = link.closest('.nav').querySelector('img');
+
+		siblings.forEach(el => {
+			if (el !== link) el.style.opacity = this;
+		});
+		logo.style.opacity = this;
+	}
+	;
+	;
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+
+const intialCoords = section1.getBoundingClientRect();
+console.log(intialCoords)
+window.addEventListener('scroll', function (e) {
+	if (window.scrollY > intialCoords.top) nav.classList.add('sticky');
+	else nav.classList.remove('sticky');
 })
 
-// document.querySelectorAll('.nav__link').forEach(function (el){
+//// document.querySelectorAll('.nav__link').forEach(function (el){
 // 	el.addEventListener('click', function (e){
 // 		e.preventDefault();
 // 		const  id = this.getAttribute('href');
